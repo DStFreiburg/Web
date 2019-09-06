cb994d0d810e3ac40cb6a2138cd6115092e4b1c7
DStFreiburg <DavidStork012@gmail.com>
2019-09-06 08:58:56 +0200

    Changed Bluetooth Filters

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function requestBluetoothDevice() {
   log('Requesting bluetooth device...');
 
   return navigator.bluetooth.requestDevice({
-    filters: [{services: [0xFFE0]}],
+    
   }).
       then(device => {
         log('"' + device.name + '" bluetooth device selected');
